38bf14c33624e7a795275a0576a9ee6ccc8abb6e
Vanessa Flores <floresv26@gmail.com>
2020-09-09 12:48:20 -0500

    Set content inset of tableview so first row can be visible and cells scroll under search bar

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -17,7 +17,8 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
 
