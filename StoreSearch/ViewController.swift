b9b5c53128e2ff355e65951a614794157e3d5cdb
Vanessa Flores <floresv26@gmail.com>
2020-09-09 12:14:25 -0500

    Project setup

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Vanessa Flores on 9/9/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
