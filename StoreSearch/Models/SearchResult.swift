50df210384a98d51cb65a02bb410a9078c472819
Vanessa Flores <floresv26@gmail.com>
2020-09-10 15:02:42 -0500

    Support better property names

--- StoreSearch/Models/SearchResult.swift
+++ StoreSearch/Models/SearchResult.swift
@@ -19,16 +19,28 @@ class SearchResult: Codable, CustomStringConvertible {
     var kind: String? = ""
     var trackPrice: Double? = 0.0
     var currency: String = ""
-    var artworkUrl60: String? = ""
-    var artworkUrl100: String? = ""
-    var trackViewUrl: String? = ""
-    var primaryGenreName: String = ""
+    var imageSmall: String? = ""
+    var imageLarge: String? = ""
+    var storeURL: String? = ""
+    var genre: String = ""
     
     var name: String {
         return trackName ?? ""
     }
     
     var description: String {
-        return "Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")\n"
+        return "Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None"), Currency: \(currency)\n"
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case storeURL = "trackViewUrk"
+        case genre = "primaryGenreName"
+        case kind
+        case artistName
+        case trackName
+        case trackPrice
+        case currency
     }
 }
