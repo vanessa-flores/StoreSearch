f2d6bbf0b12bc0fa387ec6ec177a0e3567310143
Vanessa Flores <floresv26@gmail.com>
2020-09-10 15:48:05 -0500

    Add functionality to sort in order descending

--- StoreSearch/Models/SearchResult.swift
+++ StoreSearch/Models/SearchResult.swift
@@ -111,3 +111,7 @@ class SearchResult: Codable, CustomStringConvertible {
 func < (lhs: SearchResult, rhs: SearchResult) -> Bool {
     return lhs.name.localizedStandardCompare(rhs.name) == .orderedAscending
 }
+
+func > (lhs: SearchResult, rhs: SearchResult) -> Bool {
+    return lhs.name.localizedStandardCompare(rhs.name) == .orderedDescending
+}
