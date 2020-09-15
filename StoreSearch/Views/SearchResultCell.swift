115c7376c1b9169f4c2eac7783a9955c946fe589
Vanessa Flores <floresv26@gmail.com>
2020-09-15 17:16:31 -0500

    SearchResultCell refactoring

--- StoreSearch/Views/SearchResultCell.swift
+++ StoreSearch/Views/SearchResultCell.swift
@@ -31,5 +31,17 @@ class SearchResultCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    // MARK: - Public Methods
+    
+    func configure(for result: SearchResult) {
+        nameLabel.text = result.name
+        
+        if result.artist.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", result.artist, result.type)
+        }
+    }
 
 }
