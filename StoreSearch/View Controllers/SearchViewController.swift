4042c4955c74e4ccb54c769c0685ecea42f1d7f2
Vanessa Flores <floresv26@gmail.com>
2020-09-15 17:10:44 -0500

    Remove extra space in the tableview content inset

--- StoreSearch/View Controllers/SearchViewController.swift
+++ StoreSearch/View Controllers/SearchViewController.swift
@@ -36,7 +36,7 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.contentInset = UIEdgeInsets(top: 108, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = UIEdgeInsets(top: 88, left: 0, bottom: 0, right: 0)
         
         var cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
