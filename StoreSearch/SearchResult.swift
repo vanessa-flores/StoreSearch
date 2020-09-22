f38b0d9db1615249acc4d603b69f7bd4dc9320f4
Vanessa Flores <floresv26@gmail.com>
2020-09-22 13:05:56 -0500

    Use data driven approach to matching product types

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -13,6 +13,19 @@ class ResultArray: Codable {
     var results: [SearchResult] = []
 }
 
+private let typeForKind = [
+  "album": NSLocalizedString("Album", comment: "Localized kind: Album"),
+  "audiobook": NSLocalizedString("Audio Book", comment: "Localized kind: Audio Book"),
+  "book": NSLocalizedString("Book", comment: "Localized kind: Book"),
+  "ebook": NSLocalizedString("E-Book", comment: "Localized kind: E-Book"),
+  "feature-movie": NSLocalizedString("Movie", comment: "Localized kind: Feature Movie"),
+  "music-video": NSLocalizedString("Music Video", comment: "Localized kind: Music Video"),
+  "podcast": NSLocalizedString("Podcast", comment: "Localized kind: Podcast"),
+  "software": NSLocalizedString("App", comment: "Localized kind: Software"),
+  "song": NSLocalizedString("Song", comment: "Localized kind: Song"),
+  "tv-episode": NSLocalizedString("TV Episode", comment: "Localized kind: TV Episode"),
+]
+
 class SearchResult: Codable, CustomStringConvertible {
     var artistName: String? = ""
     var trackName: String? = ""
@@ -54,29 +67,7 @@ class SearchResult: Codable, CustomStringConvertible {
     var type: String {
         let kind = self.kind ?? "audiobook"
         
-        switch kind {
-        case "album":
-            return NSLocalizedString("Album", comment: "Localized kind: Album")
-        case "audiobook":
-            return NSLocalizedString("Audio Book", comment: "Localized kind: Audio Book")
-        case "book":
-            return NSLocalizedString("Book", comment: "Localized kind: Book")
-        case "ebook":
-            return NSLocalizedString("E-Book", comment: "Localized kind: E-Book")
-        case "feature-movie":
-            return NSLocalizedString("Movie", comment: "Localized kind: Movie")
-        case "music-video":
-            return NSLocalizedString("Music Video", comment: "Localized kind: Music Video")
-        case "podcast":
-            return NSLocalizedString("Podcast", comment: "Localized kind: Podcast")
-        case "software":
-            return NSLocalizedString("App", comment: "Localized kind: App")
-        case "song":
-            return NSLocalizedString("Song", comment: "Localized kind: Song")
-        case "tv-episode":
-            return NSLocalizedString("TV Episode", comment: "Localized kind: TV Episode")
-        default:
-            return kind
+        return typeForKind[kind] ?? kind
         }        
     }
     
