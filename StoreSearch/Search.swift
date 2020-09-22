d20165ad91e09509f9d3654b38b06814e75ee33f
Vanessa Flores <floresv26@gmail.com>
2020-09-22 13:19:37 -0500

    Fix web request to include language and region

--- StoreSearch/Search.swift
+++ StoreSearch/Search.swift
@@ -83,14 +83,18 @@ class Search {
     }
     
     private func iTunesURL(searchText: String, category: Category) -> URL {
+        let locale = Locale.autoupdatingCurrent
+        let language = locale.identifier
+        let countryCode = locale.regionCode ?? "US"
         let kind = category.type
         let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         
         let urlString = "https://itunes.apple.com/search?" +
-            "term=\(encodedText)&limit=200&entity=\(kind)"
+            "term=\(encodedText)&limit=200&entity=\(kind)" +
+            "&lang=\(language)&country=\(countryCode)"
         
         let url = URL(string: urlString)
-        
+        print("URL: \(url!)")
         return url!
     }
     
